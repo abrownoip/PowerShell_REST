d93c4ce25cabb9d8de5378f89b09dafaa4194418
Anthony Brown <abrownie993@gmail.com>
2019-02-19 10:55:03 -0500

    Add files via upload

---
+++ Dell_EMC/VxRail/Get-KbArticles.ps1
@@ -0,0 +1,70 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Ignore-SelfSignedCerts
+{
+    try
+    {
+        Write-Host "Adding TrustAllCertsPolicy type." -ForegroundColor White
+        Add-Type -TypeDefinition  @"
+        using System.Net;
+        using System.Security.Cryptography.X509Certificates;
+        public class TrustAllCertsPolicy : ICertificatePolicy
+        {
+             public bool CheckValidationResult(
+             ServicePoint srvPoint, X509Certificate certificate,
+             WebRequest request, int certificateProblem)
+             {
+                 return true;
+            }
+        }
+"@
+        Write-Host "TrustAllCertsPolicy type added." -ForegroundColor White
+      }
+    catch
+    {
+        Write-Host $_ -ForegroundColor "Yellow"
+    }
+    [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+}
+
+Ignore-SelfSignedCerts
+
+
+function Get-KbArticles
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $hostname
+    )
+
+[string]$user = Read-Host "Enter username"
+[string]$pass = Read-Host "Enter password"
+
+$pair = "$($user):$($pass)"
+
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+
+$basicAuthValue = "Basic $encodedCreds"
+
+$Headers = @{
+    Authorization = $basicAuthValue
+}
+
+    $uri = "https://$hostname/rest/vxm/v1/support/kb/articles"
+    Invoke-RestMethod -Method Get -Uri $uri -Headers $Headers
+}
