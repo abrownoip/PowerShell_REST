fb48880e093e01eb702eebdd56877230cb6a0a2b
Anthony Brown <abrownie993@gmail.com>
2019-02-19 09:38:36 -0600

    Update Get-SystemVersion.ps1

--- Get-SystemVersion.ps1
+++ Get-SystemVersion.ps1
@@ -3,37 +3,32 @@
 }
 
 function Ignore-SelfSignedCerts{
-    try
-    {
+    try{
         Add-Type -TypeDefinition  @"
         using System.Net;
         using System.Security.Cryptography.X509Certificates;
-        public class TrustAllCertsPolicy : ICertificatePolicy
-        {
+        public class TrustAllCertsPolicy : ICertificatePolicy {
              public bool CheckValidationResult(
              ServicePoint srvPoint, X509Certificate certificate,
-             WebRequest request, int certificateProblem)
-             {
+             WebRequest request, int certificateProblem) {
                  return true;
             }
-        }
-"@
+        }"@
       }
-    catch
-    {
+    catch {
         write-host $_ -ForegroundColor "Yellow"
     }
     [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 }
 
-function Get-HeaderInfo{
+function Get-HeaderInfo {
     Ignore-SelfSignedCerts
     $cred = Get-Credential
     $Headers = @{'Authorization' = "Basic $([System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(-join ("$($cred.GetNetworkCredential().username)",":", "$($cred.GetNetworkCredential().password)"))))"}
     Get-SystemInfo -Headers $Headers
 }
 
-function Get-SystemInfo{
+function Get-SystemInfo {
     param(
         [Parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
         $Headers
@@ -42,12 +37,12 @@ function Get-SystemInfo{
         $vxrailSystemInfo = Invoke-RestMethod -Method Get -Uri "https://10.10.187.27/rest/vxm/v1/system" -Headers $Headers
         Remove-Variable -Name cred -ErrorAction SilentlyContinue
         Print-SystemInfo -systemInfo $vxrailSystemInfo
-    } catch [System.Net.WebException]{
+    } catch [System.Net.WebException] {
        write-host "Error : System.Net.WebException...Please check error logs for more details"
     }
 }
 
-function Print-SystemInfo{
+function Print-SystemInfo {
     param(
         [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
         $systemInfo
