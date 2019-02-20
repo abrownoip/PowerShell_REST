69a2ba83f887c0d8a9752c3bbb1829e5509927f0
Jeff Davis <14242442+DropBearHunta@users.noreply.github.com>
2019-02-20 06:39:08 +0100

    Update and rename Get-SystemVersion.ps1 to Get-VXSystemInfo.ps1

---
+++ Dell_EMC/VxRail/Get-VXSystemInfo.ps1
@@ -0,0 +1,57 @@
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-VXSystemInfo {
+    [CmdletBinding()]
+    [Alias()]
+    param (
+        # System to Connect to
+        [Parameter(Mandatory = $True,
+                   ValueFromPipeline = $True,
+                   Position=0)]
+        [Alias('Url')]
+        $HostName,
+
+        # Credentials to access system
+        [PSCredential]
+        $Credential = (Get-Credential)
+    )
+    begin {
+        $CallerErrorPreference = $ErrorActionPreference
+        Unblock-SelfSignedCerts -ErroAction 'SilentlyContinue'
+        try {
+            $ErrorActionPreference = 'Stop'
+            $Headers = @{
+                Authorization = "Basic {0}" -f [System.Convert]::ToBase64String(
+                     [System.Text.Encoding]::ASCII.GetBytes(
+                        ($Credential.Username,$Credential.GetNetworkCredential().Password) -join ":"
+                    )
+                )
+            }
+        } catch {
+            Write-Error -ErrorRecord $PSItem -ErrorAction 'Stop'
+        }
+    }
+    process {
+        try {
+            $ErrorActionPreference = 'Stop'
+            $VXRailSystemInfo = Invoke-RestMethod -Method Get -Uri "https://$HostName/rest/vxm/v1/system" -Headers $Headers
+            [PSCustomObject]@{
+                Name = $SystemInfo.Installed_Components.Name
+                Health = $SystemInfo.Health
+                CurrentVersion = $SystemInfo.Installed_Components.Current_Version
+                UpgradeStatus = $SystemInfo.Installed_Components.Upgrade_Status
+            }
+        } catch {
+            Write-Error -ErrorRecord $PSItem -ErrorAction $CallerErrorPreference
+        }
+    }
+    end {}
+}
